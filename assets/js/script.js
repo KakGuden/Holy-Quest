0be37c706c1f4f746df1630f8a88711bb4f31b86
KakGuden <Blugy123@gmail.com>
2022-12-10 14:11:52 +0100

    made start screen disapear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "start") {
+                startGame();
+            }
+        });
+    }
+});
 
+function startGame(){
+    document.getElementById('game-start').style.display = "none",
+    document.getElementById('')
 
-
-
+}
 
 function attack(){
     let enemyhpbar = document.getElementById  ("enemy-hp-bar")
