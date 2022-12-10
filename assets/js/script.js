ca97ec84c2f1d4fcdb161f5926870b1d5bac6b88
KakGuden <Blugy123@gmail.com>
2022-12-10 20:18:27 +0100

    added a new game screen and improved attacks

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,43 @@ function startGame(){
 
 }
 function options(attackType) {
-    if (attackType === "attack-buttons") {
+    if (attackType === "attack-button") {
         attack();
+    } else if (attackType === "spell-button"){
+        spell();
+    } else if (attackType === "heal-button"){
+        heal();
     }
 }
+var enemyHpFirst = document.getElementById('enemyhp-1')
+var heroHpFirst = document.getElementById('herohp-1')
+var defeated = '0';
 
 function attack(){
-    let enemyhp = document.getElementById("enemyhp")
-    enemyhp.value -= 10;
+    if (defeated == '0'){
+    enemyHpFirst.value = Number(enemyHpFirst.value) - 10;
+    if (enemyHpFirst.value == '0'){
+        enemyDead(  )
+    }
+} 
 }
+function spell(){
+    if (defeated == '0'){
+    enemyhp.value = Number(enemyhp.value) - 30;
+    if (enemyhp.value == '0'){
+        enemyDead(  )
+    }
+  }
+}
+function heal(){
+    let herohp = document.getElementById("herohp")
+    herohp.value += 30;
+}
+function enemyDead(){
+    if (defeated == '0'){
+    document.getElementById('game-screen-1').style.display = "none"
+    document.getElementById('game-screen-2').style.display = "flex"
+    defeated = 1;
+    console.log(defeated)   
+    }
+};
