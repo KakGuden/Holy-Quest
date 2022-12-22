93a8339643f39e4e4f8548fd801569933b0f78bb
KakGuden <Blugy123@gmail.com>
2022-12-22 04:35:58 +0100

    made JaveScript less cluttered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,18 @@
 /** diffrent screens */
-const gameScreenOneRef = document.getElementById('game-screen-1');
-const gameScreenTwoRef = document.getElementById('game-screen-2');
-const gameScreenThreeRef = document.getElementById('game-screen-3');
-const introOneRef = document.getElementById('intro');
-const introTwoRef = document.getElementById('intro-2');
-const introThreeRef = document.getElementById('intro-3');
-const endScreenOneRef = document.getElementById('end-screen-1');
-const endScreenTwoRef = document.getElementById('end-screen-2');
+const gameScreensRef = document.querySelectorAll('.game-screens');
+const introScreensRef = document.querySelectorAll('.intro-screens');
 const gameOptionsRef = document.getElementById('game-options');
 /** Enemy Hp */
-const enemyHpFirst = document.getElementById('enemyhp-1');
-const enemyHpFirstRef = document.getElementById("enemy-hp-number-1");
-const enemyHpSecond = document.getElementById('enemyhp-2');
-const enemyHpTwoRef = document.getElementById("enemy-hp-number-2");
-const enemyHpThird = document.getElementById('enemyhp-3');
-const enemyHpThirdRef = document.getElementById("enemy-hp-number-3");
+const enemyHpRef = document.querySelectorAll(".enemyhp")
+const enemyHpNumberRef = document.querySelectorAll(".enemyhp-number");
 /** Attack Effects */
 const enemyAttackEffect = document.querySelectorAll(".enemy-animation");
 const heroAttackEffect = document.querySelectorAll('.hero-animation');
 /** Hero Hp */
-const heroHpFirst = document.getElementById('herohp-1');
-const heroHpFirstRef = document.getElementById("hero-hp-number-1");
-const heroHpSecond = document.getElementById('herohp-2');
-const heroHpTwoRef = document.getElementById("hero-hp-number-2");
-const heroHpThird = document.getElementById('herohp-3');
-const heroHpThirdRef = document.getElementById("hero-hp-number-3");
+const heroHpRef = document.querySelectorAll('.herohp');
+const heroHpNumberRef = document.querySelectorAll(".herohp-number");
 /** Game Options */
-const cooldownAttackRef = document.getElementById("attack-button");
-const cooldownSpellRef = document.getElementById("spell-button");
-const cooldownHealRef = document.getElementById("heal-button");
+const cooldownOptionsRef = document.querySelectorAll(".options");
 let defeated = '0';
 
 
@@ -48,14 +32,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function intro(){
-    document.getElementById('game-start').style.display = "none";
-    introOneRef.style.display = "flex";
+    gameScreensRef[0].style.display = "none";
+    introScreensRef[0].style.display = "flex";
     setTimeout(startGame, 12000);
 }
 
 function startGame(){
-    introOneRef.style.display = "none";
-    gameScreenOneRef.style.display = "flex";
+    introScreensRef[0].style.display = "none";
+    gameScreensRef[1].style.display = "flex";
     gameOptionsRef.style.display = "flex";
 
 }
@@ -75,31 +59,31 @@ function options(attackType) {
 
 function attack(){
     if (defeated == '0'){
-    enemyHpFirst.value = Number(enemyHpFirst.value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
-    enemyHpFirstRef.innerHTML = enemyHpFirst.value;
+    enemyHpRef[0].value = Number(enemyHpRef[0].value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
+    enemyHpNumberRef[0].innerHTML = enemyHpRef[0].value;
     attackAnimation();
     cooldownOn();
-    if (enemyHpFirst.value == '0'){
+    if (enemyHpRef[0].value == '0'){
         enemyDead( );
     } else {
         setTimeout(enemyFirstAttack, 5000);
     }
     } else if (defeated == '1'){
-    enemyHpSecond.value = Number(enemyHpSecond.value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
-    enemyHpTwoRef.innerHTML = enemyHpSecond.value;
+    enemyHpRef[1].value = Number(enemyHpRef[1].value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
+    enemyHpNumberRef[1].innerHTML = enemyHpRef[1].value;
     attackAnimation();
     cooldownOn();
-    if (enemyHpSecond.value == '0'){
+    if (enemyHpRef[1].value == '0'){
         enemyDead(  );
         } else {
             setTimeout(enemySecondAttackOne, 5000);
         }
     } else if (defeated == '2'){
-        enemyHpThird.value = Number(enemyHpThird.value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
-        enemyHpThirdRef.innerHTML = enemyHpThird.value;
+        enemyHpRef[2].value = Number(enemyHpRef[2].value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
+        enemyHpNumberRef[2].innerHTML = enemyHpRef[2].value;
         attackAnimation();
         cooldownOn();
-        if (enemyHpThird.value == '0'){
+        if (enemyHpRef[2].value == '0'){
             enemyDead(  );
             }
          else {
@@ -109,31 +93,31 @@ function attack(){
 }
 function spell(){
     if (defeated == '0'){
-        enemyHpFirst.value = Number(enemyHpFirst.value) - 30;
-        enemyHpFirstRef.innerHTML = enemyHpFirst.value;
+        enemyHpRef[0].value = Number(enemyHpRef[0].value) - 30;
+        enemyHpNumberRef[0].innerHTML = enemyHpRef[0].value;
         cooldownOn();
         spellAnimation();
-        if (enemyHpFirst.value == '0'){
+        if (enemyHpRef[0].value == '0'){
             enemyDead(  );
         } else {
             setTimeout(enemyFirstAttack, 5000);
         }
     } else if (defeated == '1'){
-        enemyHpSecond.value = Number(enemyHpSecond.value) - 30;
-        enemyHpTwoRef.innerHTML = enemyHpSecond.value;
+        enemyHpRef[1].value = Number(enemyHpRef[1].value) - 30;
+        enemyHpNumberRef[1].innerHTML = enemyHpRef[1].value;
         cooldownOn();
         spellAnimation();
-        if (enemyHpSecond.value == '0'){
+        if (enemyHpRef[1].value == '0'){
             enemyDead(  );
             } else{
                 setTimeout(enemySecondAttackTwo, 5000);
             }
         } else if (defeated == '2'){
-            enemyHpThird.value = Number(enemyHpThird.value) - 30;
-            enemyHpThirdRef.innerHTML = enemyHpThird.value;
+            enemyHpRef[2].value = Number(enemyHpRef[2].value) - 30;
+            enemyHpNumberRef[2].innerHTML = enemyHpRef[2].value;
             spellAnimation();
             cooldownOn();
-            if (enemyHpThird.value == '0'){
+            if (enemyHpRef[2].value == '0'){
                 enemyDead(  );
                 } else{
                 setTimeout(enemyThirdAttack, 5000);
@@ -142,20 +126,20 @@ function spell(){
 }
 function heal(){
     if (defeated == '0'){
-        heroHpFirst.value = Number(heroHpFirst.value) + 30;
-        heroHpFirstRef.innerHTML = heroHpFirst.value;
+        heroHpRef[0].value = Number(heroHpRef[0].value) + 30;
+        heroHpNumberRef[0].innerHTML = heroHpRef[0].value;
         healAnimation();
         cooldownOn();
         setTimeout(enemyFirstAttack, 5000);
     } else if (defeated == '1'){
-    heroHpSecond.value = Number(heroHpSecond.value) + 30;
-    heroHpTwoRef.innerHTML = heroHpSecond.value;
+        heroHpRef[1].value = Number(heroHpRef[1].value) + 30;
+        heroHpNumberRef[1].innerHTML = heroHpRef[1].value;
         healAnimation();
         cooldownOn();
         setTimeout(enemySecondAttackThree, 5000);
     } else if (defeated == '2'){
-        heroHpThird.value = Number(heroHpThird.value) + 30;
-        heroHpThirdRef.innerHTML = heroHpThird.value;
+        heroHpRef[2].value = Number(heroHpRef[2].value) + 30;
+        heroHpNumberRef[2].innerHTML = heroHpRef[2].value;
         healAnimation();
         cooldownOn();
         setTimeout(enemyThirdAttack, 5000);
@@ -164,97 +148,97 @@ function heal(){
 /** screen changers */
 function enemyDead(){
     if (defeated == '0'){
-    gameScreenOneRef.style.display = "none";
+    gameScreensRef[1].style.display = "none";
     gameOptionsRef.style.display = "none";
-    introTwoRef.style.display = "flex";
+    introScreensRef[1].style.display = "flex";
     defeated = 1;
     cooldownOff();
     setTimeout(gameScreenTwo, 5000);
     } else if (defeated == '1'){
-    gameScreenTwoRef.style.display = "none";
+    gameScreensRef[2].style.display = "none";
     gameOptionsRef.style.display = "none";
-    introThreeRef.style.display = "flex";
+    introScreensRef[2].style.display = "flex";
     defeated = 2;
     cooldownOff();
     setTimeout(gameScreenThree, 7000);
     } else if (defeated == '2'){
-        gameScreenThreeRef.style.display = "none";
+        gameScreensRef[3].style.display = "none";
         gameOptionsRef.style.display = "none";
-        endScreenOneRef.style.display = "flex";
+        introScreensRef[3].style.display = "flex";
         defeated = 0;
         cooldownOff();
         setTimeout(endScreen, 10000);
     }
 }
 function heroDead(){
+    gameScreensRef[1].style.display = "none";
+    gameScreensRef[2].style.display = "none";
+    gameScreensRef[3].style.display = "none";
     gameOptionsRef.style.display = "none";
-    gameScreenTwoRef.style.display = "none";
-    gameScreenThreeRef.style.display = "none";
-    gameOptionsRef.style.display = "none";
-    document.getElementById('lose-screen').style.display = "flex";
+    introScreensRef[5].style.display = "flex";
 }
 function gameScreenTwo(){
-    introTwoRef.style.display = "none";
-    gameScreenTwoRef.style.display = "flex";
+    introScreensRef[1].style.display = "none";
+    gameScreensRef[2].style.display = "flex";
     gameOptionsRef.style.display = "flex";
 }
 function gameScreenThree(){
-    introThreeRef.style.display = "none";
-    gameScreenThreeRef.style.display = "flex";
+    introScreensRef[2].style.display = "none";
+    gameScreensRef[3].style.display = "flex";
     gameOptionsRef.style.display = "flex";
 }
 function endScreen(){
-    endScreenOneRef.style.display = "none";
-    endScreenTwoRef.style.display = "flex";
+    introScreensRef[3].style.display = "none";
+    introScreensRef[4].style.display = "flex";
 }
 /** Enemy attacks */
 function enemyFirstAttack(){
-    heroHpFirst.value = Number(heroHpFirst.value) - 20;
-    heroHpFirstRef.innerHTML = heroHpFirst.value;
+    heroHpRef[0].value = Number(heroHpRef[0].value) - 20;
+    heroHpNumberRef[0].innerHTML = heroHpRef[0].value;
     heroAttackEffect[0].src = "assets/images/Attack.png";
     setTimeout(animationsOff, 500);
     setTimeout(cooldownOff, 1000);
-    if (heroHpFirst.value == '0'){
+    if (heroHpRef[0].value == '0'){
         heroDead(  );
     }
 }
 function enemySecondAttackOne(){
-    heroHpSecond.value = Number(heroHpSecond.value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
-    heroHpTwoRef.innerHTML = heroHpSecond.value;
+    heroHpRef[1].value = Number(heroHpRef[1].value) - Math.floor(Math.random() * (35 - 25 + 1) + 25);
+    heroHpNumberRef[1].innerHTML = heroHpRef[1].value;
     heroAttackEffect[1].src = "assets/images/Attack.png";
     setTimeout(animationsOff, 500);
     setTimeout(cooldownOff, 1000);
-    if (heroHpSecond.value == '0'){
+    if (heroHpRef[1].value == '0'){
         heroDead(  );
     }
 }
 function enemySecondAttackTwo(){
-    heroHpSecond.value = Number(heroHpSecond.value) - 30;
-    heroHpTwoRef.innerHTML = heroHpSecond.value;
+    heroHpRef[1].value = Number(heroHpRef[1].value) - 30;
+    heroHpNumberRef[1].innerHTML = heroHpRef[1].value;
     heroAttackEffect[1].src = "assets/images/Spell.png";
     setTimeout(animationsOff, 500);
     setTimeout(cooldownOff, 1000);
-    if (heroHpSecond.value == '0'){
+    if (heroHpRef[1].value == '0'){
         heroDead(  );
     }
 }
 function enemySecondAttackThree(){
-    enemyHpSecond.value = Number(enemyHpSecond.value) + 30;
-    enemyHpTwoRef.innerHTML = enemyHpSecond.value;
+    enemyHpRef[1].value = Number(enemyHpRef[1].value) + 30;
+    enemyHpNumberRef[1].innerHTML = enemyHpRef[1].value;
     enemyAttackEffect[1].src = "assets/images/Heal.png";
     setTimeout(animationsOff, 500);
     setTimeout(cooldownOff, 1000);
-    if (heroHpSecond.value == '0'){
+    if (heroHpRef[1].value == '0'){
         heroDead(  );
     }
 }
 function enemyThirdAttack(){
-    heroHpThird.value = Number(heroHpThird.value) - 25;
-    heroHpThirdRef.innerHTML = heroHpThird.value;
+    heroHpRef[2].value = Number(heroHpRef[2].value) - 25;
+    heroHpNumberRef[2].innerHTML = heroHpRef[2].value;
     heroAttackEffect[2].src = "assets/images/Spell.png";
     setTimeout(animationsOff, 500);
     setTimeout(cooldownOff, 1000);
-    if (heroHpThird.value == '0'){
+    if (heroHpRef[2].value == '0'){
         heroDead(  );
     }
 }
@@ -309,12 +293,12 @@ function animationsOff(){
 }
 
 function cooldownOn(){
-    cooldownAttackRef.disabled = true;
-    cooldownSpellRef.disabled = true;
-    cooldownHealRef.disabled = true;
+    cooldownOptionsRef[0].disabled = true;
+    cooldownOptionsRef[1].disabled = true;
+    cooldownOptionsRef[2].disabled = true;
 }
 function cooldownOff(){
-    cooldownAttackRef.disabled = false;
-    cooldownSpellRef.disabled = false;
-    cooldownHealRef.disabled = false;
+    cooldownOptionsRef[0].disabled = false;
+    cooldownOptionsRef[1].disabled = false;
+    cooldownOptionsRef[2].disabled = false;
 }
